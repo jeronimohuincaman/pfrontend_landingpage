b250c458e75c4f5189faef1c5431fe299062600e
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2024-09-09 20:28:23 -0300

    `Entrega final` - Se agrega documentacion - Se mejora estilo en mobile - Se temrina formulario - Se crea archivo json el cual obtiene datos para el select. - Se agregan variables en :root css

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pfrontend_landingpage
 Trabajo Practico N1 donde se debe realizar una landing page y un formulario
+
+## Aclaraciones
+Profe, las tareas "Trabajo Practico #1: Parte Practica 1Tarea" y "Trabajo Practico #1, Parte Practica 2Tarea" estan en este mismo proyecto. Debe levantar el proyecto con LiveServer o similar.
+
+### Instrucciones
+1. La pagina principal es la landingpage donde cumplo los requerimientos la parte 1
+2. Si nos dirigimos al menu de navegacion y presionamos sobre "contactanos" accederemos a la tarea 2 del formulario.
+    ```http
+        http://127.0.0.1:5500/src/contactanos/index.html
+    ```
+3. Si presiona sobre el logo del ITS lo redirecciona a la landingpage.
+
+Gracias, Jerónimo Ezequiel Huincamán.
--- src/contactanos/index.js
+++ src/contactanos/index.js
@@ -1,8 +1,55 @@
 document.addEventListener('DOMContentLoaded', (e) => {
+    // Encapsulo elementos del dom para utilizarlos luego
     const hamburguesa = document.getElementById('hamburger');
     const menu_movil = document.getElementById('mobile-menu');
+    const select = document.getElementById('condicion');
+    const form = document.getElementById('form');
 
+    // Agrego evento 'onClick' al boton de hamburguesa
     hamburguesa.addEventListener('click', (e) => {
         menu_movil.classList.toggle('hidden');
     });
+
+    /**
+     * Esta funcion se encarga de recorrer un arreglo e ir armando el selector del DOM
+     */
+    function armarSelect(options) {
+        options.forEach(o => {
+            const option = document.createElement('option');
+            option.value = o.id;
+            option.textContent = o.descripcion;
+            select.appendChild(option);
+        })
+    }
+
+    // Utilizo este metodo para llamar a mi archivo json
+    fetch('/assets/opciones.json').then((res) => {
+        if (!res) {
+            return console.log('Error al cargar opciones del select');
+        }
+        return res.json();
+    }).then((opciones) => {
+        armarSelect(opciones.options); // Utilizo la funcion antes creada
+    }).catch((error) => {
+        console.log('Error al cargar opciones del select:', error);
+    }
+    );
+
+    // Agrego una escucha al evento de tipo 'submit' para poder encapsular los valores del formulario y mostrarlos por consola
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        // encapsulo los datos en un formato que pueda manipular
+        const formulario = new FormData(form);
+
+        // Variable auxiliar
+        let formjson = {};
+
+        // Por cada clave valor se la asigno a la variable auxiliar donde almacenare mis datos
+        formulario.forEach((value, key) => {
+            formjson[key] = value;
+        });
+
+        console.log(formjson)
+    })
 });
